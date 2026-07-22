d529dc3f872296e7dc6137489db66e9c22d2fb7b
rajendraprasad10 <rajendra.taidala10@gmail.com>
2026-07-22 11:29:31 +0530

    Initial FastAPI CI

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="GitHub Actions CI Demo",
+    version="1.0"
+)
+
+@app.get("/")
+def home():
+    return {
+        "message": "GitHub Actions CI Pipeline",
+        "status": "Success"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "Healthy"
+    }
