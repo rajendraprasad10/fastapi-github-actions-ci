cdef25b62b2dfe977f38bfc988c1b5e4c7555b81
rajendraprasad10 <rajendra.taidala10@gmail.com>
2026-07-22 12:20:09 +0530

    changes FastAPI CI

--- app/main.py
+++ app/main.py
@@ -1,17 +1,14 @@
 from fastapi import FastAPI
-
 app = FastAPI(
     title="GitHub Actions CI Demo",
     version="1.0"
 )
-
 @app.get("/")
 def home():
     return {
         "message": "GitHub Actions CI Pipeline",
         "status": "Success"
     }
-
 @app.get("/health")
 def health():
     return {
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,38 +2,6 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
-
-
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
-
-
-
-
-
-
-
-
-
-# from fastapi.testclient import TestClient
-# from app.main import app
-
-# client = TestClient(app)
-
-# def test_home():
-
-#     response = client.get("/")
-
-#     assert response.status_code == 200
-
-#     assert response.json()["status"] == "Success"
-
-
-# def test_health():
-
-#     response = client.get("/health")
-
-#     assert response.status_code == 200
-
-#     assert response.json()["status"] == "Healthy"
